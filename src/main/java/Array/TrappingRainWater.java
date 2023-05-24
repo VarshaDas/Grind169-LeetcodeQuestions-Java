3748e6a33a138d6042d2859913676c74f282186e
VarshaDas <varsha.das@cerner.com>
2023-05-24 23:48:08 +0530

    42. Trapping Rain Water

---
+++ src/main/java/Array/TrappingRainWater.java
@@ -0,0 +1,61 @@
+package Array;
+
+public class TrappingRainWater {
+    /**
+     * Trapping Rain Water:
+     * Given an elevation map representing the height of bars, this method calculates the total amount
+     * of rainwater that can be trapped between the bars.
+     *
+     * Intuition :
+     * We create two arrays, leftMax and rightMax, to store the maximum height from the left and right side
+     * respectively for each bar.
+     * By doing so, we can determine the maximum height that water can be trapped at each position.
+     * Then, we iterate through each bar and calculate the trapped water by taking the minimum height
+     * between the left and right maximums and subtracting the current bar's height.
+     * Finally, we sum up the trapped water for each bar and return the total amount.
+     *
+     * @param heights An array of integers representing the height of the bars.
+     * @return The total amount of rainwater trapped.
+     *
+     * Time Complexity: O(n) The code iterates through the array of heights three times: one for calculating the left maximums,
+     * one for calculating the right maximums, and one for calculating the trapped water.
+     * Since each iteration takes O(n) time, the overall time complexity is O(n).
+     *
+     * Space Complexity: O(n) The code uses two additional arrays, leftMax and rightMax,
+     * of size n to store the maximum heights from the left and right side. Hence, the space complexity is O(n).
+     *
+     * Leetcode link - https://leetcode.com/problems/trapping-rain-water/description/
+     */
+    public static int trapRainWater(int[] heights) {
+        int n = heights.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+        // Calculate the maximum height from the left for each bar
+        leftMax[0] = heights[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], heights[i]);
+        }
+        // Calculate the maximum height from the right for each bar
+        rightMax[n - 1] = heights[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], heights[i]);
+        }
+        int trappedWater = 0;
+
+        // Calculate the trapped water for each bar
+        for (int i = 0; i < n; i++) {
+            int minHeight = Math.min(leftMax[i], rightMax[i]);
+            trappedWater += minHeight - heights[i];
+        }
+        return trappedWater;
+    }
+
+    public static void main(String[] args) {
+        int[] input = {4,2,0,3,2,5};
+        System.out.println(trapRainWater(input));
+
+    }
+}
