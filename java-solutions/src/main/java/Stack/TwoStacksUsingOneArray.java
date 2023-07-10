185f6ca93dae3d6d84918a4b297d986da1a6e06c
vd056735 <varsha.das@cerner.com>
2023-07-10 23:42:28 +0530

    two stacks

--- java-solutions/src/main/java/Stack/TwoStacksUsingOneArray.java
+++ java-solutions/src/main/java/Stack/TwoStacksUsingOneArray.java
@@ -20,7 +20,7 @@ public class TwoStacksUsingOneArray {
     public void push1(int num) {
         // Write your code here
         if (top1 < top2 - 1) {
-            arr[top1++] = num;
+            arr[++top1] = num;
         }
 
     }
@@ -28,18 +28,50 @@ public class TwoStacksUsingOneArray {
     // Push in stack 2.
     public void push2(int num) {
         // Write your code here
+        if (top1 < top2 - 1) {
+            arr[--top2] = num;
+        }
 
     }
 
     // Pop from stack 1 and return popped element.
     public int pop1() {
         // Write your code here
+        if(top1 == -1){
+            return -1;
+        }
+        return arr[top1--];
 
     }
 
     // Pop from stack 2 and return popped element.
     public int pop2() {
         // Write your code here
+        if(top2 == maxsize) {
+            return -1;
+        }
+        return arr[top2--];
+
+    }
+
+    public void display(){
+        for(int i = 0;i<maxsize;i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        TwoStacksUsingOneArray twoStacks = new TwoStacksUsingOneArray(5);
+        twoStacks.push1(1);
+        twoStacks.push2(2);
+        twoStacks.push1(3);
+        twoStacks.push2(4);
+        twoStacks.push1(5);
+        twoStacks.display();
+
+
+        System.out.println("Popped element from Stack 1: " + twoStacks.pop1());  // Output: 5
+        System.out.println("Popped element from Stack 2: " + twoStacks.pop2());  // Output: 4
 
     }
 
