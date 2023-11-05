b4ea30b9366ef987d053f5ab5a87aff68d4668f3
Varsha <vdaskol@gmail.com>
2023-11-05 23:43:05 +0530

    daily temperatures leetcode

--- java-solutions/src/main/java/Stack/DailyTemperatures.java
+++ java-solutions/src/main/java/Stack/DailyTemperatures.java
@@ -1,7 +1,27 @@
 package Stack;
 
+import java.util.Stack;
+
 /*
-similar to next greater element
- */
+
+Leetcode link - https://leetcode.com/problems/daily-temperatures/description/
+
+*/
 public class DailyTemperatures {
+        public int[] dailyTemperatures(int[] temperatures) {
+            int n = temperatures.length;
+            int[] result = new int[n];
+            Stack<Integer> stack = new Stack<>();
+
+            for (int i = 0; i < n; i++) {
+                while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                    int idx = stack.pop();
+                    result[idx] = i - idx;
+                }
+                stack.push(i);
+            }
+
+            return result;
+        }
 }
+
